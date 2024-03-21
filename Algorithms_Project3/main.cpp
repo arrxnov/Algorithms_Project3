8e591407d861923d1f3cd4d078c91a4f1a9021cc
arrxnov <70920682+arrxnov@users.noreply.github.com>
2024-03-21 03:25:37 -0400

    Passes 6/7

--- Algorithms_Project3/main.cpp
+++ Algorithms_Project3/main.cpp
@@ -51,102 +51,6 @@ void unionSets(Member* x, Member* y)
 	linkSets(findSet(x), findSet(y));
 }
 
-// ------------------------------//
-//     ALGORITHMIC FUNCTIONS     // 
-// ------------------------------//
-
-bool sameComponent(Member* u, Member* v)
-{
-	if (findSet(u) == findSet(v))
-		return true;
-	else
-		return false;
-}
-
-void connectedComponents(Member** G, int n, int m, int k)
-{
-	for (int i = 0; i < n * m * k; i++)
-	{
-		Member* v = G[i];
-		if (v != NULL) makeSet(v);
-	}
-
-	for (int i = 0; i < n * m * k; i++)
-	{
-		Member* v = G[i];
-		Member** nodes = new Member* [6];
-		
-		if (v == NULL) continue;
-
-		// +/- n axis
-		if (v->id % n < n - 1)
-		{
-			nodes[0] = G[v->id + 1];
-		}
-		else
-		{
-			nodes[0] = NULL;
-		}
-		if (v->id % n > 0)
-		{
-			nodes[1] = G[v->id - 1];
-		}
-		else
-		{
-			nodes[1] = NULL;
-		}
-
-		// +/- m axis
-		if ((v->id / n % m) < m - 1)
-		{
-			nodes[2] = G[v->id + n];
-		}
-		else
-		{
-			nodes[2] = NULL;
-		}
-		if (v->id / n > 0)
-		{
-			nodes[3] = G[v->id - n];
-		}
-		else
-		{
-			nodes[3] = NULL;
-		}
-
-		// +/- k axis
-		if ((v->id / m / n) % k < k - 1)
-		{
-			nodes[4] = G[v->id + m * n];
-		}
-		else
-		{
-			nodes[4] = NULL;
-		}
-		if (v->id / m / n > 0)
-		{
-			nodes[5] = G[v->id - m * n];
-		}
-		else
-		{
-			nodes[5] = NULL;
-		}
-
-		for (int j = 0; j < 6; j++)
-		{
-			Member* u = nodes[j];
-			if (u == NULL || v == NULL)
-			{
-				continue;
-			}
-			if (findSet(u) != findSet(v))
-			{
-				unionSets(u, v);
-			}
-		}
-	}	
-}
-
 //-------------------------------//
 //             MAIN              //
 //-------------------------------//
@@ -154,54 +58,120 @@ void connectedComponents(Member** G, int n, int m, int k)
 int main()
 {
 	using namespace std;
-	int k, l, m, n;
-	cin >> n >> m >> k>> l;
-
+	int k, l, m, n, setCount = 0, numMonthsDisconnectedFromTheMainCOBOLCluster = 0;
+	cin >> n >> m >> k >> l;
 	int** monarchies = new int*[l];
 
-	for (int i = 0; i < l; i++)
+	for (int i = l - 1; i >= 0; i--)
 	{
-		int p;
+		int p, input;
 		cin >> p;
-		monarchies[i] = new int[p];
+		monarchies[i] = new int[p + 1];
 		monarchies[i][0] = p;
 		for (int j = 1; j <= p; j++)
 		{
-			cin >> monarchies[i][j];
+			cin >> input;
+			monarchies[i][j] = input;
 		}
 	}
 
 	// build empire
 	Member** empire = new Member*[n * m * k];
+
 	for (int i = 0; i < n * m * k; i++)
 	{
-		Member* newMember = new Member;
-		newMember->id = i;
-		empire[i] = newMember;
+		empire[i] = NULL;
 	}
 
-	for /* each monarchy */ (int i = 1; i < l; i++)
+	for /* each monarchy */ (int i = 0; i < l; i++)
 	{
-		// remove pieces of empire one by one
+		// add pieces of empire one by one
 		for /* each dominion per monarchy */ (int j = 1; j <= monarchies[i][0]; j++)
 		{
-			empire[monarchies[i][j]] = NULL;
-		}
+			 Member* newNode = new Member;
+			 newNode->id = monarchies[i][j];
+			 empire[newNode->id] = newNode;
+			 makeSet(newNode);
+			 setCount++;
+			 // Check neighbors
+			 int local_height = newNode->id / (m * n);
+			 int temp = newNode->id;
+			 temp = newNode->id % (m * n);
+			 int local_column = temp / n;
+			 int local_row = temp % n;
+			 Member** nodes = new Member*[6];
+			 // +/- n axis
+			 if (local_row < n - 1)
+			 {
+				 if (empire[newNode->id + 1] != NULL 
+					 && findSet(newNode) != findSet(empire[newNode->id + 1]))
+				 {
+					 unionSets(newNode, empire[newNode->id + 1]);
+					setCount--;
+				 }
+			 }
 
-		// test if sets are disjoint
-		connectedComponents(empire, n, m, k);
-		Member* curr = NULL;
-		for (int j = 0; j < n * m * k; j++)
+			 if (local_row > 0)
+			 {
+				 if (empire[newNode->id - 1] != NULL
+					 && findSet(newNode) != findSet(empire[newNode->id - 1]))
+				 {
+					 unionSets(newNode, empire[newNode->id - 1]);
+					 setCount--;
+				 }
+			 }
+
+			 // +/- m axis
+			 if (local_column < m - 1)
+			 {
+				 if (empire[newNode->id + n] != NULL
+					 && findSet(newNode) != findSet(empire[newNode->id + n]))
+				 {
+					 unionSets(newNode, empire[newNode->id + n]);
+					 setCount--;
+				 }
+			 }
+
+			 if (local_column > 0)
+			 {
+				 if (empire[newNode->id - n] != NULL
+					 && findSet(newNode) != findSet(empire[newNode->id - n]))
+				 {
+					 unionSets(newNode, empire[newNode->id - n]);
+					 setCount--;
+				 }
+			 }
+
+			 // +/- k axis
+			 if (local_height < k - 1)
+			 {
+				 if (empire[newNode->id + n * m] != NULL
+					 && findSet(newNode) != findSet(empire[newNode->id + m * n]))
+				 {
+					 unionSets(newNode, empire[newNode->id + m * n]);
+					 setCount--;
+				 }
+			 }
+
+			 if (local_height > 0)
+			 {
+				 if (empire[newNode->id - n * m] != NULL
+					 && findSet(newNode) != findSet(empire[newNode->id - m * n]))
+				 {
+					 unionSets(newNode, empire[newNode->id - m * n]);
+					 setCount--;
+				 }
+			 } 
+
+			 empire[monarchies[i][j]] = newNode;
+		}
+		if (setCount > 1)
 		{
-			if (empire[j] == NULL) continue;
-			if (curr == NULL) curr = empire[j]->p;
-			else if (curr != empire[j]->p)
-			{
-				cout << "[+] Broken at " << i << endl;
-				break;
-			}
+			numMonthsDisconnectedFromTheMainCOBOLCluster++;
 		}
 	}
+
+	cout << numMonthsDisconnectedFromTheMainCOBOLCluster << endl;
 
 	for (int i = 0; i < n * m * k; i++)
 	{
