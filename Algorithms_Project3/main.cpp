a69864ab18cfe3dcfb22d14f2234af47a216de9e
arrxnov <70920682+arrxnov@users.noreply.github.com>
2024-03-20 21:33:28 -0400

    Fix some stuff

--- Algorithms_Project3/main.cpp
+++ Algorithms_Project3/main.cpp
@@ -1,12 +1,152 @@
 #include <iostream>
+#include <vector>
+
+//-------------------------------//
+//           OBJECTS             //
+//-------------------------------//
+
+class Member {};
+
+class Member
+{
+public:
+	Member *p;
+	int rank;
+	int id;
+};
 
 //-------------------------------//
 //  POSITIONAL HELPER FUNCTIONS  //
 //-------------------------------//
 
+void makeSet(Member* x)
+{
+	x->p = x;
+	x->rank = 0;
+}
+
+Member* findSet(Member* x)
+{
+	if (x != x->p)
+	{
+		x->p = findSet(x->p);
+	}
+	return x->p;
+}
+
+void unionSets(Member* x, Member* y)
+{
+	linkSets(findSet(x), findSet(y));
+}
+
+void linkSets(Member* x, Member* y)
+{
+	if (x->rank > y->rank)
+	{
+		y->p = x;
+	}
+	else
+	{
+		if (x->rank == y->rank)
+		{
+			y->rank++;
+		}
+	}
+}
+
 // ------------------------------//
 //     ALGORITHMIC FUNCTIONS     // 
 // ------------------------------//
+
+bool sameComponent(Member* u, Member* v)
+{
+	if (findSet(u) == findSet(v))
+		return true;
+	else
+		return false;
+}
+
+void connectedComponents(Member** G, int n, int m, int k)
+{
+	for (int i = 0; i < n * m * k; i++)
+	{
+		Member* v = G[i];
+		if (v != NULL) makeSet(v);
+	}
+
+	for (int i = 0; i < n * m * k; i++)
+	{
+		Member* v = G[i];
+		Member** nodes = new Member * [6];
+		
+		// +/- n axis
+		if (v->id % n < n - 1)
+		{
+			nodes[0] = G[v->id + 1];
+		}
+		else
+		{
+			nodes[0] = NULL;
+		}
+		if (v->id % n > 0)
+		{
+			nodes[1] = G[v->id - 1];
+		}
+		else
+		{
+			nodes[1] = NULL;
+		}
+
+		// +/- m axis
+		if (v->id / n % m < m - 1)
+		{
+			nodes[2] = G[v->id + n];
+		}
+		else
+		{
+			nodes[2] = NULL;
+		}
+		if (v->id / n > 0)
+		{
+			nodes[3] = G[v->id - n];
+		}
+		else
+		{
+			nodes[3] = NULL;
+		}
+
+		// +/- k axis
+		if (v->id / m / n % k < k - 1)
+		{
+			nodes[4] = G[v->id + m * n];
+		}
+		else
+		{
+			nodes[4] = NULL;
+		}
+		if (v->id / m / n > 0)
+		{
+			nodes[5] = G[v->id - m * n];
+		}
+		else
+		{
+			nodes[5] = NULL;
+		}
+
+		for (int j = 0; j < 6; j++)
+		{
+			Member* u = nodes[j];
+			if (u == NULL || v == NULL)
+			{
+				continue;
+			}
+			if (findSet(u) != findSet(v))
+			{
+				unionSets(u, v);
+			}
+		}
+	}	
+}
 
 //-------------------------------//
 //             MAIN              //
@@ -15,9 +155,8 @@
 int main()
 {
 	using namespace std;
-	int k, l, m, n, numProblems;
-	cin >> numProblems;
-	cin >> n >> m >> l >> k;
+	int k, l, m, n;
+	cin >> n >> m >> k>> l;
 
 	int** monarchies = new int*[l];
 
@@ -26,13 +165,33 @@ int main()
 		int p;
 		cin >> p;
 		monarchies[i] = new int[p];
+		monarchies[i][0] = p;
 		for (int j = 0; j < p; j++)
 		{
 			cin >> monarchies[i][j];
 		}
 	}
 
+	// build empire
+	Member** empire = new Member*[n * m * k];
+	for (int i = 0; i < n * m * k; i++)
+	{
+		Member* newMember = new Member;
+		newMember->id = i;
+		empire[i] = newMember;
+	}
 
+	for (int i = 0; i < l; i++)
+	{
+		// remove pieces of empire one by one
+		for (int j = 0; j < monarchies[i][0]; i++)
+		{
+			empire[monarchies[i][j]] = NULL;
+		}
+
+		// test if sets are disjoint
+		connectedComponents(empire, n, m, k);
+	}
 
 	return 0;
 }
